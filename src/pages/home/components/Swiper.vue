7c26b51f7cae3cd4b1d8a8d5947a0f6502011252
thinkaboutme <464352933@qq.com>
2018-10-10 10:18:09 +0800

    index swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="swiper">
+    <swiper :options="swiperOption">
+    <!-- slides -->
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl" />
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/ff/1bf7d3a4f6591002.jpg_750x200_40809f2a.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .swiper >>> .swiper-pagination-bullet-active
+    background:#fff
+  .swiper
+    overflow:hidden
+    width:100%
+    height:0
+    padding-bottom:26.67%
+    background:#eee
+    .swiper-img
+      width:100%
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import Home from '@/pages/home/Home'
 import '@/assets/style/reset.css'
 import '@/assets/style/border.css'
 import '@/assets/style/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 Vue.use(Router)
-
+Vue.use(VueAwesomeSwiper)
 export default new Router({
   routes: [
     {
